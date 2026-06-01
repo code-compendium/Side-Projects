f777b3a582b30e0c0cc9b5c4c4d7d1c40bd7027c
Code Compendium <compendiumcode@gmail.com>
2026-06-01 15:23:38 +0200

    feat: - initialize pokemonApi.js - HomePage.jsx to test async/await fetching

--- Pokemon/src/pages/HomePage.jsx
+++ Pokemon/src/pages/HomePage.jsx
@@ -1,4 +1,21 @@
+import { useEffect } from "react";
+import { getPokemonList } from "../api/pokemonApi";
+
 export default function HomePage() {
+  useEffect(() => {
+    async function loadPokemon() {
+      try {
+        const data = await getPokemonList();
+
+        console.log(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    loadPokemon();
+  }, []);
+
   return (
     <main>
       <h1>Pokédex</h1>
